4657721f84b65cd8d8e7fcdbf2f7bf76952bafe1
Stephanie Murphy <smurphy@mas.local>
2021-05-18 13:24:38 -0400

    navbar complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,23 @@
 import React, {useEffect} from "react"
 
 function Navbar(props) {
-    
+    useEffect(() => {
+        document.title = props.currentPage.page
+    })
+
+    return(
+        <nav>
+            <ul>
+                {props.pages.map(page => (
+                    <li key = {page.page}> 
+                    <span onClick = {() => props.setCurrentPage(page)}>
+                    {page.page}
+                    </span>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    )
 }
+
+export default Navbar
