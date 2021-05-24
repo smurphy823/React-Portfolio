fc4b5f0e1d82caae373ea14670490794a0351750
Stephanie Murphy <stephaniemurphy588@yahoo.com>
2021-05-24 16:25:48 -0400

    Intro

---
+++ src/components/Intro.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Intro() {
+    return(
+        <section className="intro" id="home">
+        <h1 className="section__title section__title--intro">
+            Hi, I am Stephanie <strong>Murphy</strong>
+        </h1>
+        <p className="section__subtitle section__subtitle--intro">Beginner Full Stack Developer</p>
+        
+        </section>
+    );
+}
+
+export default Intro;
