409d63352a98e91f379b8bff6e1bb6a214a3109a
Stephanie Murphy <stephaniemurphy588@yahoo.com>
2021-05-24 11:26:37 -0400

    About page

---
+++ src/components/About.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+function About() {
+    return(
+        <section className="about-me" id="about">
+        <h2 className="section__title section__title--about">Who I am</h2>
+        <p className="section__subtitle section__subtitle--about">Beginner Full Stack Developer from Syracuse, NY</p>
+       
+        <div className="about-me__body">
+               <p>My name is Stephanie Murphy, I am a Beginner Full Stack Developer from Syracuse, NY.  I love and enjoy the process of becoming a proffessional Web Developer.  Learning different languages are very intersting. </p>
+               <p>Html CSS, SQL, NoSQL, are the languages I've experienced so far and ofcorse Javascript which interest me the most. </p>
+               <p>Reactjs and Nodejs are the frameworks that I have most recently learned and worked with.</p>
+       </div>
+       
+        </section>
+    )
+}
+
+export default About;
