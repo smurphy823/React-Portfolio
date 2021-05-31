58d414fa73fae5f453d29e0e315bc8de8766d037
Stephanie Murphy <stephaniemurphy588@yahoo.com>
2021-05-31 13:30:27 -0400

    portfolio function complete

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,10 +4,40 @@ import Project from "./Project"
 function Portfolio() {
     const [homeworks] = useState([
         {
-            title: "NoteTaker",
-            deployed: "https://smurphy823.github.io/NoteTaker/",
-            github: "https://github.com/smurphy823/NoteTaker",
-            description: "This is a Note Taker"
+            title: "WorkdayScheduler",
+            deployed: "https://github.com/smurphy823/Work-Day-Scheduler.git",
+            github: "https://smurphy823.github.io/Work-Day-Scheduler/",
+            description: "A simple calendar app for scheduling your work day"
+        },
+        {
+            title: "WeatherDashboard",
+            deployed: "https://github.com/smurphy823/Weather-Dashboard.git",
+            github: "https://smurphy823.github.io/Weather-Dashboard/",
+            description: "A weather dashboard app"
+        },
+        {
+            title: "KitchenBlogger",
+            deployed: "https://github.com/vinhkhamhuynh/Kitchen-Blogger.git",
+            github: "https://kitchenblogger.herokuapp.com/",
+            description: "This is a recipe sharing blog post application"
+        },
+        {
+            title: "FitnessTracker",
+            deployed: "https://github.com/smurphy823/Workout-Tracker.git",
+            github: "https://stark-tor-23631.herokuapp.com/",
+            description: "A daily tracking fitness app"
+        },
+        {
+            title: "BudgetTracker",
+            deployed: "https://shielded-retreat-13776.herokuapp.com/",
+            github: "https://smurphy823.github.io/Online-Offline-Budget-Trackers/",
+            description: "This application is a simple budget tracker"
+        },
+        {
+            title: "UserDirectory",
+            deployed: "https://github.com/smurphy823/Employee-Directory/tree/main",
+            github: "https://smurphy823.github.io/Employee-Directory/",
+            description: "A react app that renders from a random user API and displays users in a company"
         },
     ])
 
